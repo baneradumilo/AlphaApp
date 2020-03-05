b406a157a9d72d8d12105c596d154340c2b89fc3
baneradumilo <41484058+baneradumilo@users.noreply.github.com>
2020-03-05 16:09:23 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -9,3 +9,93 @@ app.controller('Ctrl', function($scope) {
     name: 'Type Text Here'
   };
 });
+
+app.controller('SelectLocalCtrl', function($scope, $filter) {
+  $scope.user = {
+    status: 2
+  }; 
+
+  $scope.statuses = [
+    {value: 1, text: 'status1'},
+    {value: 2, text: 'status2'},
+    {value: 3, text: 'status3'},
+    {value: 4, text: 'status4'}
+  ]; 
+
+  $scope.showStatus = function() {
+    var selected = $filter('filter')($scope.statuses, {value: $scope.user.status});
+    return ($scope.user.status && selected.length) ? selected[0].text : 'Not set';
+  };
+});
+
+app.controller('EditableFormCtrl', function($scope, $filter, $http) {
+  $scope.user = {
+    id: 1,
+    name: 'awesome user',
+    status: 2,
+    group: 4,
+    groupName: 'admin'
+  }; 
+
+  $scope.statuses = [
+    {value: 1, text: 'status1'},
+    {value: 2, text: 'status2'},
+    {value: 3, text: 'status3'},
+    {value: 4, text: 'status4'}
+  ]; 
+
+  $scope.groups = [
+  ];
+  $scope.loadGroups = function() {
+    return $scope.groups.length ? null : $http.get('/groups').success(function(data) {
+      $scope.groups = data;
+    });
+  };
+
+  $scope.showGroup = function() {
+    if($scope.groups.length) {
+      var selected = $filter('filter')($scope.groups, {id: $scope.user.group});
+      return selected.length ? selected[0].text : 'Not set';
+    } else {
+      return $scope.user.groupName;
+    }
+  };
+
+  $scope.checkName = function(data) {
+    if (data !== 'awesome' && data !== 'error') {
+      return "Username should be `awesome` or `error`";
+    }
+  };
+
+  $scope.saveUser = function() {
+    // $scope.user already updated!
+    return $http.post('/saveUser', $scope.user).error(function(err) {
+      if(err.field && err.msg) {
+        // err like {field: "name", msg: "Server-side error for this username!"} 
+        $scope.editableForm.$setError(err.field, err.msg);
+      } else { 
+        // unknown error
+        $scope.editableForm.$setError('name', 'Unknown error!');
+      }
+    });
+  };
+});
+
+app.controller('Html5InputsCtrl', function($scope) {
+  $scope.user = {
+    email: 'email@example.com',
+    tel: '123-45-67',
+    number: 29,
+    range: 10,
+    url: 'http://example.com',
+    search: 'blabla',
+    color: '#6a4415',
+    date: null,
+    time: new Date(1970, 0, 1, 12, 30),
+    month: null,
+    week: null,
+    password: 'password',
+    datetimeLocal: null,
+    file: null
+  };  
+});
