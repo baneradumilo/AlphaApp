8eb97990880b94a2dcfc84eeb05aac94e78face2
baneradumilo <41484058+baneradumilo@users.noreply.github.com>
2020-03-09 17:13:54 +0100

    Add files via upload

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,66 +1,18 @@
-var app = angular.module('myApp', []);
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
 
-app.directive('sidebarToggle', [function() {
-	return {
-		restrict: 'A',
-		link: function (scope, iElement, iAttrs) {
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
 
-			scope.element = iElement[0];
-			scope.body = document.body;
-
-			scope.element.addEventListener("click", function() {
-				var _element = document.getElementById(iAttrs.sidebarToggle);
-				_element.classList.toggle("sidebar--is-open");
-
-				var _bgSidebar = document.getElementById(iAttrs.sidebarToggle + "-bg");
-				_bgSidebar.classList.toggle("sidebar-bg--is-visible");
-
-				//control body
-				if (scope.body.style.overflowY == "auto") {
-					scope.body.style.overflowY = "hidden";
-				} else {
-					scope.body.style.overflowY = "auto";
-				}
-			});
-
-		}
-	};
-}]);
-
-app.directive('sidebar', ['$compile', function($compile) {
-	return {
-		restrict: 'E',
-		link: function (scope, iElement, iAttrs) {
-
-			scope.body = document.body;
-			scope.element = iElement[0];
-
-			//renderize sidebar
-			scope.element.style.display = "block";
-
-			//add width on sidebar
-			function isNumber(n) {
-				return !isNaN(parseFloat(n)) && isFinite(n);
-			}
-
-			if (isNumber(iAttrs.size)) {
-				scope.element.style.maxWidth = iAttrs.size + 'px';
-			} else {
-				scope.element.style.maxWidth = iAttrs.size;
-			}
-
-			//add class to position on sidebar
-			scope.element.classList.add("sidebar--" + iAttrs.position);
-
-			//renderize bg-sidebar
-			var bgSidebar = document.createElement("div");
-			bgSidebar.setAttribute("class", "sidebar-bg");
-			bgSidebar.setAttribute("id", iAttrs.id + "-bg");
-			bgSidebar.setAttribute("sidebar-toggle", iAttrs.id);
-			scope.body.appendChild(bgSidebar);
-
-			$compile(bgSidebar)(scope);
-
-		}
-	};
-}]);
+function myFunction() {
+  var x = document.getElementById("newContact");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
